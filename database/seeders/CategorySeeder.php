6ae5b189153732aca23013b51f98c7e73fa16be1
mtungij <jamesmtungi2013@gmail.com>
2026-01-07 17:20:11 +0300

    first commit

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -15,11 +15,13 @@ class CategorySeeder extends Seeder
         Category::create(['name' => 'Beer', 'parent_id' => $alcohol->id]);
         Category::create(['name' => 'Wine', 'parent_id' => $alcohol->id]);
         Category::create(['name' => 'Spirits', 'parent_id' => $alcohol->id]);
-        Category::create(['name' => 'Liqueurs', 'parent_id' => $alcohol->id]);
+        Category::create(['name' => 'Whiskey', 'parent_id' => $alcohol->id]);
+     
 
         // Non-alcohol subcategories
         Category::create(['name' => 'Soft Drinks', 'parent_id' => $nonAlcohol->id]);
         Category::create(['name' => 'Energy Drinks', 'parent_id' => $nonAlcohol->id]);
-        Category::create(['name' => 'Water & Juice', 'parent_id' => $nonAlcohol->id]);
+        Category::create(['name' => 'Water', 'parent_id' => $nonAlcohol->id]);
+           Category::create(['name' => 'Juice', 'parent_id' => $nonAlcohol->id]);
     }
 }
